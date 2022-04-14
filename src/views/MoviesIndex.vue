a28b18c71526df740e8d7be71005305ca2de1a7c
Brandon Lin <bwlin93@gmail.com>
2022-04-13 23:04:44 -0500

    adds movieIndex page, routes, links

---
+++ src/views/MoviesIndex.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- List of movies  -->
+    <div>
+      <h2>All movies:</h2>
+      <div v-for="movie in movies" :key="movie.id">
+        {{ movie.title }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      movies: [],
+    };
+  },
+  created: function () {
+    this.moviesIndex();
+  },
+  methods: {
+    moviesIndex: function () {
+      axios.get("http://localhost:3000/movies.json").then((response) => {
+        this.movies = response.data;
+      });
+    },
+  },
+};
+</script>
