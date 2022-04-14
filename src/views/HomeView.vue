cb358f384c93a7a40e3427cfa3ffd2d8d250faff
Brandon Lin <bwlin93@gmail.com>
2022-04-13 20:12:05 -0500

    fixes update likn to use currentmovie.id

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,102 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h1>Welcome to the movie page</h1>
+    <!-- Create a new movie  -->
+    <div>
+      <button @click="modalMovieCreate">Create a new movie</button>
+      <dialog id="movie-create">
+        <div>
+          <h2>Create a new movie:</h2>
+          <form>
+            <p><input type="text" v-model="newMovieParams.title" placeholder="Title" /></p>
+            <p><input type="text" v-model="newMovieParams.plot" placeholder="Plot" /></p>
+            <p><input type="text" v-model="newMovieParams.year" placeholder="Year" /></p>
+            <p><input type="text" v-model="newMovieParams.director" placeholder="Director" /></p>
+          </form>
+          <button v-on:click="movieCreate()">Submit</button>
+        </div>
+      </dialog>
+    </div>
+
+    <!-- List of movies  -->
+    <div>
+      <h2>All movies:</h2>
+      <div v-for="movie in movies" :key="movie.id">
+        {{ movie.title }}
+
+        <!-- Show / Edit Movies  -->
+        <button v-on:click="modalMovieShow(movie)">More details</button>
+        <dialog id="movie-show">
+          <form>
+            <p><input type="text" v-model="currentMovie.title" placeholder="Title" /></p>
+            <p><input type="text" v-model="currentMovie.plot" placeholder="Plot" /></p>
+            <p><input type="text" v-model="currentMovie.year" placeholder="Year" /></p>
+            <p><input type="text" v-model="currentMovie.director" placeholder="Director" /></p>
+          </form>
+          <button @click="movieUpdate()">Submit Changes</button>
+        </dialog>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      movies: [],
+      newMovieParams: {},
+      currentMovie: {},
+    };
+  },
+  created: function () {
+    this.moviesIndex();
+  },
+  methods: {
+    moviesIndex: function () {
+      axios.get("http://localhost:3000/movies.json").then((response) => {
+        this.movies = response.data;
+      });
+    },
+    movieCreate: function () {
+      axios.post("http://localhost:3000/movies.json", this.newMovieParams).then((response) => {
+        this.movies.push(response);
+      });
+      this.newMovieParams.title = null;
+      this.newMovieParams.year = null;
+      this.newMovieParams.director = null;
+      this.newMovieParams.plot = null;
+      this.moviesIndex();
+    },
+    movieUpdate: function () {
+      axios
+        .patch("http://localhost:3000/movies/" + this.currentMovie.id + ".json", this.currentMovie)
+        .then((response) => {
+          console.log(response.data);
+          this.moviesIndex();
+        })
+        .catch((error) => console.log(error.response));
+    },
+
+    modalMovieShow: function (movie) {
+      this.currentMovie = movie;
+      document.getElementById("movie-show").showModal();
+    },
+    modalMovieCreate: function () {
+      document.getElementById("movie-create").showModal();
+    },
   },
 };
 </script>
+
+<style>
+#movie-show {
+  width: 500px;
+}
+
+input {
+  width: 450px;
+}
+</style>
