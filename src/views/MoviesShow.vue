08838c4898f2314a42bce2c93da0795e6998fe2f
Brandon Lin <bwlin93@gmail.com>
2022-04-14 09:27:37 -0500

    creates show page with redirect to index page

---
+++ src/views/MoviesShow.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <!-- List of movies  -->
+    <div>
+      <div>
+        <p>Title: {{ currentMovie.title }}</p>
+        <p>Plot: {{ currentMovie.plot }}</p>
+        <p>Year: {{ currentMovie.year }}</p>
+        <p>Director: {{ currentMovie.director }}</p>
+      </div>
+      <button @click="moviesIndexRedirect()">Return to all movies</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      currentMovie: {},
+    };
+  },
+  created: function () {
+    this.moviesShow();
+  },
+  methods: {
+    moviesShow: function () {
+      axios.get("http://localhost:3000/movies/" + this.$route.params.id + ".json").then((response) => {
+        this.currentMovie = response.data;
+      });
+    },
+    moviesIndexRedirect: function () {
+      this.$router.push("/movies");
+    },
+  },
+};
+</script>
